62103223a3ef819ece95ef52f536206e38e4c677
Spudoku <calebisntshort@gmail.com>
2025-05-27 19:27:13 -0700

    More recursion testing
    30 minutes

--- Thumb 2 Programs/heap.s
+++ Thumb 2 Programs/heap.s
@@ -55,8 +55,11 @@ _heap_init_done
 ; R3: entire
 ; R4: half
 ; R5: midpoint
-; R6: heap_addr
-; R0 through R6 are reserved, EXCEPT when returning a pointer in R0!
+; R6: act_entire_size
+; R7: act_half_size
+; R8: heap_addr
+; R0 through R7 are reserved, EXCEPT when returning a pointer in R0!
+; Store addresses in R8 when possible
 		
 _ralloc
 		PUSH	{lr}		; store link register
@@ -71,31 +74,46 @@ _ralloc
 		
 		; defining midpoint
 		ADD		R5, R1, R4		; int midpoint = left + half;
-		
-		; defining heap_addr
-		MOV		R6, #0x0; 	int heap_addr = NULL;
-		
+
 		; definining act_entire_size
-		LSL		R7, R3, #4 	; int act_entire_size = entire * 16;
+		LSL		R6, R3, #4 	; int act_entire_size = entire * 16;
 		
 		; defining act_half_size
-		LSL		R8, R4, #4 	; int act_half_size = half * 16;
-		CMP		R0, R8
+		LSL		R7, R4, #4 	; int act_half_size = half * 16;
+		; defining heap_addr
+		MOV		R8, #0x0; 	int heap_addr = NULL;
+		
 		; if size > act_half_size, go to base case(s)
+		CMP		R0, R7
+		
 		BHI		_ralloc_base
 		; else recurse left
-		PUSH	{R0-R6}		; store all variables
-		POP		{R0-R6}
-		
-		
+		PUSH	{R0-R7}		; store all variables
+		; compute midpoint - mcb_ent_sz
+		LDR		R9, =MCB_ENT_SZ
+		SUB		R2, R5, R9 	;midpoint - mcb_ent_sz 
+		BL		_ralloc
+		MOV		R8, R0		; store result into R6
+		POP		{R0-R7}
+		CMP		R8, #0x0	; if heap_addr != NULL
+		BNE		_ralloc_left_good
+		; recurse right
+		; left = midpoint
+		MOV		R1, R5
+		BL		_ralloc
+		B		_ralloc_return_heap_addr
+_ralloc_left_good
+		; split parent
+		B	_ralloc_return_heap_addr
 _ralloc_base
-		
+		; load (array[m2a(left)]) into R7
+		; for now just go to null
 _ralloc_return_null
 		MOV		R0, #0x0
 		POP		{pc}
 		
 _ralloc_return_heap_addr
-		MOV		R0, R6
+		MOV		R0, R8
 		POP		{pc}		; push a link register into the program counter
 		;BX		lr
 		; END _ralloc
